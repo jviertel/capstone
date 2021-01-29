af09b04d1d7548d5f4a4deeab76669372dba1091
jviertel <jvierte02@gmail.com>
2021-01-29 00:09:47 -0600

    Set to live server database

--- models.py
+++ models.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import json
 
-database_path = os.environ.get('DATABASE_URL_TESTING')
+database_path = os.environ.get('DATABASE_URL_LIVE')
 db = SQLAlchemy()
 
 def setup_db(app, database_path=database_path):
