ab3a58336446d0965e3c0468a8cb81bac84b4046
jviertel <jvierte02@gmail.com>
2021-01-29 00:46:45 -0600

    Style guidelines

--- auth.py
+++ auth.py
@@ -9,13 +9,17 @@ AUTH0_DOMAIN = os.environ.get('AUTH0_DOMAIN')
 ALGORITHMS = os.environ.get('ALGORITHMS')
 API_AUDIENCE = os.environ.get('API_AUDIENCE')
 
-#AuthError Exception
+# AuthError Exception
+
+
 class AuthError(Exception):
     def __init__(self, error, status_code):
         self.error = error
         self.status_code = status_code
 
-#Get Authorization header from token
+# Get Authorization header from token
+
+
 def get_token_auth_header():
     if 'Authorization' not in request.headers:
         raise AuthError('No header present', 401)
@@ -27,10 +31,12 @@ def get_token_auth_header():
     if (len(split_header) != 2) or (split_header[0].lower() != 'bearer'):
         raise AuthError('Malformed header', 401)
 
-    token =  split_header[1]
+    token = split_header[1]
     return token
 
-#Check permission 
+# Check permission
+
+
 def check_permissions(permission, payload):
     if 'permissions' not in payload:
         raise AuthError('Permissions not in payload', 400)
@@ -39,7 +45,9 @@ def check_permissions(permission, payload):
         raise AuthError('Required permission not in permissions list', 401)
     return True
 
-#Decode and verify JWT
+# Decode and verify JWT
+
+
 def verify_decode_jwt(token):
     key_url = urlopen(f'https://{AUTH0_DOMAIN}/.well-known/jwks.json')
     keys = json.loads(key_url.read())
@@ -58,25 +66,28 @@ def verify_decode_jwt(token):
                 'n': key['n'],
                 'e': key['e']
             }
-    
+
     if rsa_key:
         try:
             payload = jwt.decode(token, rsa_key, algorithms=ALGORITHMS,
-                audience=API_AUDIENCE, issuer='https://' + AUTH0_DOMAIN + '/')
+                                 audience=API_AUDIENCE,
+                                 issuer='https://' + AUTH0_DOMAIN + '/')
             return payload
-        
+
         except jwt.ExpiredSignatureError:
             raise AuthError('Token expired', 401)
 
-        except jwt.JWTClaimsError: 
+        except jwt.JWTClaimsError:
             raise AuthError('Invalid claims', 401)
 
         except Exception:
             raise AuthError('Cannot parse token', 400)
-    
+
     raise AuthError('Cannot find key', 400)
 
-#Decorator method to require authorization
+# Decorator method to require authorization
+
+
 def requires_auth(permission=''):
     def requires_auth_decorator(f):
         @wraps(f)
@@ -87,4 +98,4 @@ def requires_auth(permission=''):
             return f(payload, *args, **kwargs)
 
         return wrapper
-    return requires_auth_decorator 
+    return requires_auth_decorator
